73c0e4d72be0094575b03466fe062a4f9e60ecec
ujagar <ujagarsinghlodhi@gmail.com>
2023-05-20 14:55:48 +0530

    created structure of project

--- api/src/app.js
+++ api/src/app.js
@@ -1,5 +1,9 @@
 const express = require("express");
+require("dotenv").config();
 
+const PORT = process.env.PORT || 3001
+console.log("PORT: ", PORT);
 const app = express();
 
-app.listen()
+app.listen(PORT, () => { console.log(`APP is running on port ${ PORT } `) });
+
