22193bf799bd98e521b897dfa2e46eaec5b878a5
ujagar <ujagarsinghlodhi@gmail.com>
2023-05-21 12:27:35 +0530

    Develoed user sign up , registration and profile api , developed goals crud

--- api/src/app.js
+++ api/src/app.js
@@ -1,9 +1,37 @@
-const express = require("express");
-require("dotenv").config();
+const path = require('path');
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const port = process.env.PORT || 3001;
 
-const PORT = process.env.PORT || 3001
-console.log("PORT: ", PORT);
+connectDB();
+// console.log("PORT: ", PORT);
 const app = express();
 
-app.listen(PORT, () => { console.log(`APP is running on port ${ PORT } `) });
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.get("/", (req, res) => { res.send("Base url is calling.."); });
+
+app.use('/api/goals', require('./routes/goalRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
+// Serve frontend
+/* if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+      )
+    );
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+  }
+ */  
+app.use(errorHandler);
+  
+app.listen(port, () => { console.log(`APP is running on port ${ port } `) });
 
---
+++ api/src/routes/goalRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+const {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require('../controllers/goalController')
+
+const { protect } = require('../middleware/authMiddleware')
+
+router.route('/').get(protect, getGoals).post(protect, setGoal)
+router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal)
+
+// router.route('/').get(getGoals).post(setGoal)
+// router.route('/:id').delete(deleteGoal).put(updateGoal)
+
+module.exports = router
